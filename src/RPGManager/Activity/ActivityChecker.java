21103fbd7d0643af8e276412b773ca01513c45c5
Danni Sunne <etnichols55@gmail.com>
2017-12-08 22:08:22 -0800

    Activity Check Base

--- src/RPGManager/Activity/ActivityChecker.java
+++ src/RPGManager/Activity/ActivityChecker.java
@@ -1,4 +1,87 @@
 package RPGManager.Activity;
 
+import com.tumblr.jumblr.JumblrClient;
+import RPGManager.Main;
+import com.tumblr.jumblr.types.Post;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 public class ActivityChecker {
+    private JumblrClient client;
+    private SimpleDateFormat jumblrFormat;
+    private final int DAYSTILINACTIVE = 7;
+
+    public ActivityChecker() {
+        client = new JumblrClient(Main.OAUTHKEY, Main.OAUTHSECRET);
+        client.setToken(Main.OAUTHTOKEN, Main.OAUTHTOKENSECRET);
+        jumblrFormat = new SimpleDateFormat("yyyy-MM-dd");
+    }
+
+    // Tumblr is considered inactive (false) if it has not posted in seven days.
+    // Can be editted by changing DAYSTILINACTIVE
+    public boolean isActive(String blogName) {
+        boolean result = false;
+
+        Post latestPost = getPost(blogName);
+        Date postDate = new Date();
+        String gmtString = "";
+
+        postDate.equals(formatDate(latestPost, gmtString));
+
+        result = postedInDateRange(postDate);
+
+        System.out.println("isActive{" + result + "}");
+
+        return result;
+    }
+
+    private Post getPost(String blogName) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "text");
+        params.put("limit", 1);
+        params.put("filter", "text");
+        List<Post> posts = client.blogPosts(blogName, params);
+
+        return posts.get(0);
+    }
+
+    private Date formatDate(Post latestPost, String gmtString) {
+        Date result = new Date();
+
+        System.out.println(latestPost.getDateGMT());
+        gmtString = latestPost.getDateGMT();
+        gmtString = gmtString.substring(0, 10);
+        System.out.println(gmtString);
+
+        try {
+            result = jumblrFormat.parse(gmtString);
+        } catch(ParseException e) {
+            e.printStackTrace();
+            System.out.println("Error Parsing Date\nActivityChecker.java");
+        }
+
+        System.out.println("formatDate{" + result + "}");
+
+        return result;
+    }
+
+    private boolean postedInDateRange(Date postDate) {
+        boolean result = false;
+        long DAY_IN_MS = 1000 * 60 * 60 * 24;
+        Date checkDate = new Date(System.currentTimeMillis() - (DAYSTILINACTIVE * DAY_IN_MS));
+
+        if(postDate.after(checkDate)) {
+            result = true;
+        }
+
+        System.out.println("postedInDateRange{" + result + "}");
+
+        return result;
+    }
 }
--- src/RPGManager/Main.java
+++ src/RPGManager/Main.java
@@ -1,8 +1,16 @@
 package RPGManager;
 
+import RPGManager.Activity.ActivityChecker;
+
 public class Main {
+    public static final String OAUTHKEY = "ll6VScG2GkDnl4d9nBjyBU0uc90FSPxz3AaF74yJHxmXBJEmrC";
+    public static final String OAUTHSECRET = "WaNrENiCan93RZq9kKEF03fdsQLxmTzanAPHwiomelkd4MaatR";
+    public static final String OAUTHTOKEN = "kaQt8UFkwG9WoUEV93bQ0zFGUnp6Stawy8OyoG0WfCqnMHfYhv";
+    public static final String OAUTHTOKENSECRET = "5GsV5pXT8TLxqe9ZGWIBdg6JUv3GV2gpbYnvTpPnEorU1kSKfA";
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        ActivityChecker test = new ActivityChecker();
+        test.isActive("brokenrebel.tumblr.com");
     }
 }
